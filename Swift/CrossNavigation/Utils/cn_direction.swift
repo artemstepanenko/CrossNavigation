ffa4e7f239b6a7491af3b05995ed18243d4072e0
Nicolas Purita <nicospuri@gmail.com>
2014-11-05 11:14:23 -0300

    Initial commit for Swift migration

---
+++ Swift/CrossNavigation/Utils/cn_direction.swift
@@ -0,0 +1,58 @@
+//
+//  cn_direction.swift
+//  CrossNavigationDemo
+//
+//  Created by Nicolas Purita on 11/5/14.
+//
+//
+
+import Foundation
+
+enum CNDirection {
+    
+    case CNDirectionNone
+    case CNDirectionLeft
+    case CNDirectionTop
+    case CNDirectionRight
+    case CNDirectionBottom
+    
+    func CNDirectionGetOpposite() -> CNDirection {
+        
+        var opposite: CNDirection = .CNDirectionNone
+        
+        switch self {
+        case .CNDirectionLeft:
+            opposite = .CNDirectionRight
+        case .CNDirectionRight:
+            opposite = .CNDirectionLeft
+        case .CNDirectionTop:
+            opposite = .CNDirectionBottom
+        case .CNDirectionBottom:
+            opposite = .CNDirectionTop
+        default:
+            opposite = .CNDirectionNone
+        }
+        
+        return opposite
+    }
+    
+    func CNDirectionOppositeToDirection(direction: CNDirection) -> Bool {
+    
+        let oppositeDirection = direction.CNDirectionGetOpposite()
+        
+        if self == .CNDirectionNone || oppositeDirection == .CNDirectionNone {
+            return false
+        }
+        
+        return self == oppositeDirection
+    }
+    
+    func CNDirectionIsHorizontal() -> Bool {
+        return self == .CNDirectionLeft || self == .CNDirectionRight
+    }
+    
+    func CNDirectionIsVertical() -> Bool {
+        return self == .CNDirectionTop || self == .CNDirectionBottom
+    }
+    
+}
