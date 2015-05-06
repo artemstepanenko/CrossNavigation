79224a1df0952ea949c2ead5661013de3dc6a128
Artem Stepanenko <artem.stepanenko.1@gmail.com>
2015-05-07 00:53:36 +0200

    imports are added

--- CrossNavigation/InteractiveTransition/CNInteractiveTransition.h
+++ CrossNavigation/InteractiveTransition/CNInteractiveTransition.h
@@ -21,6 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <Foundation/Foundation.h>
 #import "cn_direction.h"
 
 @class CNViewController;
--- CrossNavigation/Segue/Performer/CNSeguePerformer.h
+++ CrossNavigation/Segue/Performer/CNSeguePerformer.h
@@ -21,6 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+#import <UIKit/UIKit.h>
 #import "cn_direction.h"
 
 @interface CNSeguePerformer : NSObject
