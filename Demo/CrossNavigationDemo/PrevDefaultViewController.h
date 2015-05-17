cdbfc7549613411449377e57634d6e4207faa2ba
Artem Stepanenko <artem.stepanenko.1@gmail.com>
2015-05-17 17:33:22 +0200

    Issue is fixed

---
+++ Demo/CrossNavigationDemo/PrevDefaultViewController.h
@@ -0,0 +1,13 @@
+//
+//  PrevDefaultViewController.h
+//  CrossNavigationDemo
+//
+//  Created by Artem Stepanenko on 5/17/15.
+//  Copyright (c) 2015 Artem Stepanenko. All rights reserved.
+//
+
+#import "CNViewController.h"
+
+@interface PrevDefaultViewController : CNViewController
+
+@end
---
+++ Demo/CrossNavigationDemo/RegularViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegularViewController.h
+//  CrossNavigationDemo
+//
+//  Created by Artem Stepanenko on 5/17/15.
+//  Copyright (c) 2015 Artem Stepanenko. All rights reserved.
+//
+
+#import "CNViewController.h"
+
+@interface RegularViewController : CNViewController
+
+@end
